0ad93c9ad5e18f4ee8cb5457f6ccf95189087e80
thomasyu888 <thomas.yu@sagebase.org>
2021-01-13 19:36:45 +0800

    Test finding new files

--- test/test_monitor.py
+++ test/test_monitor.py
@@ -1,4 +1,7 @@
 """Test monitor module"""
+from unittest.mock import Mock, patch
+
+import pandas as pd
 from synapseclient import Project
 
 from synapsemonitor import monitor
@@ -24,3 +27,26 @@ class TestGetEpochStart:
         self.project.lastAuditTimeStamp = None
         epochtime = monitor.get_epoch_start(self.project, 864000000)
         assert epochtime == 768960000
+
+
+def test_find_new_files():
+    project = Project(id="syn2222", name="testing")
+    syn = Mock()
+    table_query_results = Mock()
+    query_results = {"id": ["syn23333"], "name": ["test"],
+                     'currentVersion': [2], 'modifiedOn': [1000000000],
+                     'modifiedBy': [333333], 'type': "file"}
+    query_resultsdf = pd.DataFrame(query_results)
+    expecteddf = pd.DataFrame({
+        "id": ["syn23333"], "name": ["test"],
+        'currentVersion': [2], 'modifiedOn': ["Jan/12/1970 13:46"],
+        'modifiedBy': ["user"], 'type': "file",
+        'project': 'syn2222',
+        'projectName': 'testing'
+    })
+    with patch.object(syn, "tableQuery", return_value=table_query_results),\
+         patch.object(table_query_results, "asDataFrame", return_value=query_resultsdf),\
+         patch.object(syn, "getUserProfile", return_value={"userName": "user"}):
+
+        resultdf = monitor.find_new_files(syn, project, "syn44444", 222222)
+        assert resultdf.equals(expecteddf)
