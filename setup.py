b65de8e620ceeda3ba22f30fef61971e8711b529
thomasyu888 <thomas.yu@sagebase.org>
2022-01-13 19:41:16 +0800

    Add in version script

--- setup.py
+++ setup.py
@@ -1,28 +1,9 @@
-"""Setup"""
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# figure out the version
-# about = {}
-# here = os.path.abspath(os.path.dirname(__file__))
-# with open(os.path.join(here, "synapsemonitor", "__version__.py")) as f:
-#     exec(f.read(), about)
+version = {}
+with open("synapsemonitor/__version__.py") as fp:
+    exec(fp.read(), version)
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(name='synapsemonitor',
-      # version=about["__version__"],
-      version="0.0.2",
-      description='Synapse monitoring',
-      url='https://github.com/Sage-Bionetworks/synapseMonitor',
-      author='Larsson Omberg, Thomas Yu',
-      author_email='thomasyu888@gmail.com',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      license='Apache',
-      packages=find_packages(),
-      zip_safe=False,
-      python_requires='>=3.6, <3.9',
-      entry_points={'console_scripts': ['synapsemonitor = synapsemonitor.__main__:main']},
-      install_requires=['synapseclient', 'pandas'])
+setup(
+    version=version["__version__"]
+)
