0df8984ec6314022f1099ac31196b4d302f41e56
thomasyu888 <thomas.yu@sagebase.org>
2022-01-13 19:46:13 +0800

    Create version in __init__.py

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
 from setuptools import setup
 
-version = {}
-with open("synapsemonitor/__version__.py") as fp:
-    exec(fp.read(), version)
-
-setup(
-    version=version["__version__"]
-)
+setup()
--- synapsemonitor/__init__.py
+++ synapsemonitor/__init__.py
@@ -1,2 +1,3 @@
-import importlib.metadata
-__version__ = importlib.metadata.version('synapsemonitor')
+# create version in __init__.py
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+__version__ = "0.0.2"
