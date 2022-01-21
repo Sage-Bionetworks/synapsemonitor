9a4420a65e9ccab9dc515bb3b62b4a477dfab2ca
thomasyu888 <thomas.yu@sagebase.org>
2022-01-21 14:11:13 +0800

    Fix

--- synapsemonitor/actions.py
+++ synapsemonitor/actions.py
@@ -6,6 +6,7 @@ from . import monitor
 
 
 class SynapseAction(ABC):
+    """Base synapse action class"""
     def __init__(self, syn: Synapse, syn_id: str, days: int = 1) -> None:
         self.syn = syn
         self.syn_id = syn_id
@@ -22,3 +23,29 @@ class SynapseAction(ABC):
         )
         action_result = self._action(modified_entities)
         return action_result
+
+
+class EmailAction(SynapseAction):
+    """This action emails specified users with modified entities"""
+    def __init__(self, syn: Synapse, syn_id: str, days: int = 1,
+                 users: list = None,
+                 email_subject: str = "New Synapse Files"):
+        self.users = users
+        self.email_subject = email_subject
+        super().__init__(syn=syn, syn_id=syn_id, days=days)
+
+    def _action(self, modified_entities: list) -> list:
+        # get user ids
+        user_ids = monitor._get_user_ids(self.syn, self.users)
+
+        # TODO: Add function to beautify email message
+
+        # Prepare and send Message
+        if modified_entities:
+            self.syn.sendMessage(
+                user_ids,
+                self.email_subject,
+                ", ".join(modified_entities),
+                contentType="text/html",
+            )
+        return modified_entities
