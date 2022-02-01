a3aaf37cb10cd5c375bbd882f1c8d0616036147e
Haley Hunter-Zinck <haley.hunterzinck@sagebase.org>
2022-01-31 19:16:02 -0800

    remove test for unimplemented function

--- test/test_monitor.py
+++ test/test_monitor.py
@@ -6,7 +6,7 @@ from unittest.mock import Mock, patch
 
 import pandas as pd
 import pytest
-from synapseclient import EntityViewSchema, Project, Folder, File, Entity
+from synapseclient import Synapse, EntityViewSchema, Project, Folder, File, Entity
 
 from synapsemonitor import monitor
 import synapsemonitor
@@ -14,47 +14,56 @@ import synapsemonitor
 
 class TestModifiedEntitiesFileView:
     """Test modifying entities"""
+
     def setup_method(self):
         self.syn = Mock()
         self.table_query_results = Mock()
         query_results = {
-            "id": ["syn23333"], "name": ["test"],
-            'currentVersion': [2], 'modifiedOn': [1000000000],
-            'createdOn': [1000000000],
-            'modifiedBy': [333333], 'type': "file",
-            'projectId': ['syn55555']
+            "id": ["syn23333"],
+            "name": ["test"],
+            "currentVersion": [2],
+            "modifiedOn": [1000000000],
+            "createdOn": [1000000000],
+            "modifiedBy": [333333],
+            "type": "file",
+            "projectId": ["syn55555"],
         }
         self.query_resultsdf = pd.DataFrame(query_results)
-        self.expecteddf = pd.DataFrame({
-            "id": ["syn23333"], "name": ["test"],
-            'currentVersion': [2],
-            'modifiedOn': ["1970-01-12 05:46:40-08:00"],
-            'createdOn': ["1970-01-12 05:46:40-08:00"],
-            'modifiedBy': ["user"], 'type': "file",
-            'projectId': 'syn55555'
-        })
-        self.expecteddf['createdOn'] = self.expecteddf['createdOn'].astype(
-            'datetime64[ns, US/Pacific]'
+        self.expecteddf = pd.DataFrame(
+            {
+                "id": ["syn23333"],
+                "name": ["test"],
+                "currentVersion": [2],
+                "modifiedOn": ["1970-01-12 05:46:40-08:00"],
+                "createdOn": ["1970-01-12 05:46:40-08:00"],
+                "modifiedBy": ["user"],
+                "type": "file",
+                "projectId": "syn55555",
+            }
         )
-        self.expecteddf['modifiedOn'] = self.expecteddf['modifiedOn'].astype(
-            'datetime64[ns, US/Pacific]'
+        self.expecteddf["createdOn"] = self.expecteddf["createdOn"].astype(
+            "datetime64[ns, US/Pacific]"
+        )
+        self.expecteddf["modifiedOn"] = self.expecteddf["modifiedOn"].astype(
+            "datetime64[ns, US/Pacific]"
         )
 
     def test__render_fileview(self):
         """Test rendering of file view"""
-        with patch.object(self.syn, "getUserProfile",
-                          return_value={"userName": "user"}) as patch_get:
-            rendereddf = monitor._render_fileview(self.syn,
-                                                  self.query_resultsdf)
+        with patch.object(
+            self.syn, "getUserProfile", return_value={"userName": "user"}
+        ) as patch_get:
+            rendereddf = monitor._render_fileview(self.syn, self.query_resultsdf)
             patch_get.assert_called_once_with(333333)
             assert rendereddf.equals(self.expecteddf)
 
     def test__find_modified_entities_fileview(self):
         """Patch finding modified entities"""
-        with patch.object(self.syn, "tableQuery",
-                          return_value=self.table_query_results) as patch_q,\
-            patch.object(self.table_query_results, "asDataFrame",
-                         return_value=self.query_resultsdf) as patch_asdf:
+        with patch.object(
+            self.syn, "tableQuery", return_value=self.table_query_results
+        ) as patch_q, patch.object(
+            self.table_query_results, "asDataFrame", return_value=self.query_resultsdf
+        ) as patch_asdf:
             # patch.object(monitor, "_render_fileview",
             #              return_value=self.expecteddf) as patch_render:
             modified_list = monitor._find_modified_entities_fileview(
@@ -74,14 +83,12 @@ class TestModifiedEntitiesFileView:
 def test__find_modified_entities_file_modified():
     """Patch finding modified entities no modified"""
     syn = Mock()
-    date_mod = datetime.now().replace(tzinfo=tz.tzutc()).strftime(
-        "%Y-%m-%dT%H:%M:%S.%fZ"
+    date_mod = (
+        datetime.now().replace(tzinfo=tz.tzutc()).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
     )
     entity = File("test", "syn234", modifiedOn=date_mod)
     with patch.object(syn, "get", return_value=entity) as patch_get:
-        modified_list = monitor._find_modified_entities_file(
-            syn, "syn234", days=1
-        )
+        modified_list = monitor._find_modified_entities_file(syn, "syn234", days=1)
         patch_get.assert_called_once_with("syn234", downloadFile=False)
         assert modified_list == ["syn234"]
 
@@ -91,14 +98,10 @@ def test__find_modified_entities_file_none():
     syn = Mock()
     # create a modified date that is in the past
     old_modified = datetime.now() - timedelta(days=3)
-    date_mod = old_modified.replace(tzinfo=tz.tzutc()).strftime(
-        "%Y-%m-%dT%H:%M:%S.%fZ"
-    )
+    date_mod = old_modified.replace(tzinfo=tz.tzutc()).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
     entity = File("test", "syn234", modifiedOn=date_mod)
     with patch.object(syn, "get", return_value=entity) as patch_get:
-        modified_list = monitor._find_modified_entities_file(
-            syn, "syn234", days=1
-        )
+        modified_list = monitor._find_modified_entities_file(syn, "syn234", days=1)
         patch_get.assert_called_once_with("syn234", downloadFile=False)
         assert modified_list == []
 
@@ -106,8 +109,9 @@ def test__find_modified_entities_file_none():
 def test__get_user_ids_none():
     """Test getting logged in user profile when no users specified"""
     syn = Mock()
-    with patch.object(syn, "getUserProfile",
-                      return_value={"ownerId": "111"}) as patch_get:
+    with patch.object(
+        syn, "getUserProfile", return_value={"ownerId": "111"}
+    ) as patch_get:
         user_ids = monitor._get_user_ids(syn, None)
         patch_get.assert_called_once_with()
         assert user_ids == ["111"]
@@ -116,45 +120,24 @@ def test__get_user_ids_none():
 def test__get_user_ids():
     """Test getting user profiles ids"""
     syn = Mock()
-    with patch.object(syn, "getUserProfile",
-                      return_value={"ownerId": "111"}) as patch_get:
+    with patch.object(
+        syn, "getUserProfile", return_value={"ownerId": "111"}
+    ) as patch_get:
         user_ids = monitor._get_user_ids(syn, [1, "username"])
         patch_get.has_calls([mock.call(1), mock.call("username")])
         assert user_ids == ["111", "111"]
 
 
 @pytest.mark.parametrize(
-    "entity",
-    [
-        Project(id="syn12345", parentId="syn3333"),
-        Folder(id="syn12345", parentId="syn3333")
-    ]
-)
-def test_find_modified_entities_not_implemented(entity):
-    """Test not implemented entity types"""
-    syn = Mock()
-    with pytest.raises(NotImplementedError, match=".not supported yet"),\
-         patch.object(syn, "get", return_value=entity):
-        monitor.find_modified_entities(
-            syn=syn, syn_id="syn12345", days=1
-        )
-
-
-@pytest.mark.parametrize(
-    "entity, entity_type",
-    [
-        (Entity(id="syn12345", parentId="syn3333"), "Entity")
-    ]
+    "entity, entity_type", [(Entity(id="syn12345", parentId="syn3333"), "Entity")]
 )
 def test_find_modified_entities_unsupported(entity, entity_type):
     """Test unsupported entity types"""
     syn = Mock()
-    with pytest.raises(ValueError,
-                       match=f".+synapseclient.entity.{entity_type}'> not supported"),\
-         patch.object(syn, "get", return_value=entity):
-        monitor.find_modified_entities(
-            syn=syn, syn_id="syn12345", days=1
-        )
+    with pytest.raises(
+        ValueError, match=f".+synapseclient.entity.{entity_type}'> not supported"
+    ), patch.object(syn, "get", return_value=entity):
+        monitor.find_modified_entities(syn=syn, syn_id="syn12345", days=1)
 
 
 def test_find_modified_entities_supported():
@@ -162,12 +145,10 @@ def test_find_modified_entities_supported():
     entity = EntityViewSchema(id="syn12345", parentId="syn3333")
     syn = Mock()
     empty = pd.DataFrame()
-    with patch.object(syn, "get", return_value=entity) as patch_get,\
-         patch.object(monitor, "_find_modified_entities_fileview",
-                      return_value=empty) as patch_mod:
-        value = monitor.find_modified_entities(
-            syn=syn, syn_id="syn12345", days=1
-        )
+    with patch.object(syn, "get", return_value=entity) as patch_get, patch.object(
+        monitor, "_find_modified_entities_fileview", return_value=empty
+    ) as patch_mod:
+        value = monitor.find_modified_entities(syn=syn, syn_id="syn12345", days=1)
         patch_get.assert_called_once_with("syn12345", downloadFile=False)
         patch_mod.assert_called_once()
         assert empty.equals(value)
@@ -175,21 +156,29 @@ def test_find_modified_entities_supported():
 
 class TestMonitoring:
     """Test monitoring function, includes integration test"""
+
     def setup_method(self):
         self.syn = Mock()
 
     def test_monitoring_fail_integration(self):
         """Test all monitoring functions are called"""
         modified_list = ["syn2222", "syn33333"]
-        with patch.object(monitor, "find_modified_entities",
-                          return_value=modified_list) as patch_find,\
-             patch.object(monitor, "_get_user_ids",
-                          return_value=[111]) as patch_get_user,\
-             patch.object(self.syn, "sendMessage") as patch_send:
-            monitor.monitoring(self.syn, "syn12345", users=["2222", "fooo"],
-                               email_subject="new subject", days=15)
+        with patch.object(
+            monitor, "find_modified_entities", return_value=modified_list
+        ) as patch_find, patch.object(
+            monitor, "_get_user_ids", return_value=[111]
+        ) as patch_get_user, patch.object(
+            self.syn, "sendMessage"
+        ) as patch_send:
+            monitor.monitoring(
+                self.syn,
+                "syn12345",
+                users=["2222", "fooo"],
+                email_subject="new subject",
+                days=15,
+            )
             patch_find.assert_called_once_with(syn=self.syn, syn_id="syn12345", days=15)
             patch_get_user.assert_called_once_with(self.syn, ["2222", "fooo"])
-            patch_send.assert_called_once_with([111], "new subject",
-                                               "syn2222, syn33333",
-                                               contentType='text/html')
+            patch_send.assert_called_once_with(
+                [111], "new subject", "syn2222, syn33333", contentType="text/html"
+            )
