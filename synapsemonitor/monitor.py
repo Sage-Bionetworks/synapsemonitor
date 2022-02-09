917dccae189486d515d6eba3a9b1e76ab46dba50
Haley Hunter-Zinck <haley.hunterzinck@sagebase.org>
2022-02-09 08:20:03 -0800

    only traverse containers to limit recursive calls

--- synapsemonitor/monitor.py
+++ synapsemonitor/monitor.py
@@ -139,11 +139,16 @@ def _traverse(
 
     synid_children = syn.getChildren(parent=synid_root, includeTypes=include_types_mod)
     for synid_child in synid_children:
-        synid_desc.extend(
-            _traverse(
-                syn=syn, synid_root=synid_child["id"], include_types=include_types
+        entity = syn.get(synid_child["id"], downloadFile=False)
+        entity_type = entity["concreteType"].split(".")[-1].lower().replace("entity", "")
+        if entity_type in ['folder','project']:
+            synid_desc.extend(
+                _traverse(
+                    syn=syn, synid_root=synid_child["id"], include_types=include_types
+                )
             )
-        )
+        else:
+            synid_desc.append(synid_child["id"])
 
     # only requested entity types
     entity = syn.get(synid_root, downloadFile=False)
